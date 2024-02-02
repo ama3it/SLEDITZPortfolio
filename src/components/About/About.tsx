b2891550514e1fbaaad839f182c0e357cfcf0bdd
adhikari-amit <adhikariamit2001@gmail.com>
2024-02-02 17:01:38 +0530

    Fix Before Preview

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,23 +1,22 @@
 const About = () => {
   return (
-    <div className="container relative md:pb-24 pb-16">
-    <div className="grid md:grid-cols-12 grid-cols-1 items-center gap-6">
-      <div className="lg:col-span-5 md:col-span-6">
-        <div className="relative">
-          <div className="relative flex justify-end">
-            <iframe
-              width="560"
-              height="415"
-              className="rounded-xl"
-              src="https://www.youtube.com/embed/nqIAKW92TBM?si=xfpndmS2dub0FqtU"
-              title="YouTube video player"
-           
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-              allowFullScreen
-            ></iframe>
+    <div className="container relative md:pb-24 pb-16" id="about">
+      <div className="grid md:grid-cols-12 grid-cols-1 items-center gap-6">
+        <div className="lg:col-span-5 md:col-span-6">
+          <div className="relative">
+            <div className="relative flex justify-end">
+              <iframe
+                width="560"
+                height="415"
+                className="rounded-xl"
+                src="https://www.youtube.com/embed/nqIAKW92TBM?si=xfpndmS2dub0FqtU"
+                title="About Me"
+                allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                allowFullScreen
+              ></iframe>
+            </div>
           </div>
         </div>
-      </div>
 
         <div className="lg:col-span-7 md:col-span-6">
           <div className="lg:ms-5">
@@ -50,20 +49,20 @@ const About = () => {
               </div>
             </div>
 
-          <div className="mt-4">
-            <div className="flex justify-between mb-2">
-              <span className="text-slate-400 dark:text-white/60">
-                Photography
-              </span>
-              <span className="text-slate-400 dark:text-white/60">79%</span>
+            <div className="mt-4">
+              <div className="flex justify-between mb-2">
+                <span className="text-slate-400 dark:text-white/60">
+                  Photography
+                </span>
+                <span className="text-slate-400 dark:text-white/60">79%</span>
+              </div>
+              <div className="w-full bg-gray-100 dark:bg-gray-800 rounded-full h-[6px]">
+                <div
+                  className="bg-blue-500 h-[6px] rounded-full"
+                  style={{ width: "79%" }}
+                ></div>
+              </div>
             </div>
-            <div className="w-full bg-gray-100 dark:bg-gray-800 rounded-full h-[6px]">
-              <div
-                className="bg-blue-500 h-[6px] rounded-full"
-                style={{ width: "79%" }}
-              ></div>
-            </div>
-          </div>
 
             <div className="mt-4">
               <div className="flex justify-between mb-2">
@@ -86,4 +85,4 @@ const About = () => {
   );
 };
 
-export default About
+export default About;
