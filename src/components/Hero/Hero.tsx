c457adeac194f3ef1a758e80a7b9b2a5a389d069
adhikari-amit <adhikariamit2001@gmail.com>
2024-02-03 14:00:26 +0530

    PricingTable Type Declaration

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,4 +1,3 @@
-import { FaFileArrowDown } from "react-icons/fa6";
 import { MdOutlineKeyboardDoubleArrowDown } from "react-icons/md";
 import "./Hero.css";
 
---
+++ src/components/PricingTable/List.tsx
@@ -0,0 +1,8 @@
+
+const List = ({ children }:{children:string}) => {
+    return (
+      <p className="text-base text-body-color dark:text-dark-6">{children}</p>
+    );
+  };
+  
+  export default List
