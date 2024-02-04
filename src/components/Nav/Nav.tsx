195e203be8919e5664fe2e21c7c8523d5546a2be
adhikari-amit <adhikariamit2001@gmail.com>
2024-02-04 12:53:18 +0530

    Social links attached

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,19 +1,26 @@
 import "./Nav.css";
 
-import slEditz from "../../assets/img/SlEditz.png"
+import slEditz from "../../assets/img/SlEditz.png";
 import { useState } from "react";
+import {
+  FaFacebookF,
+  FaInstagram,
+  FaLinkedinIn,
+  FaYoutube,
+} from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
 
 const Nav = () => {
   const [mobileNav, setMobileNav] = useState<boolean>(false);
-  const [submenu,setSubmenu]=useState<boolean>(false)
+  const [submenu, setSubmenu] = useState<boolean>(false);
 
   const navToggle = () => {
     setMobileNav((prevState) => !prevState);
   };
 
-  const submenuToogle=()=>{
-    setSubmenu((prevState)=>!prevState)
-  }
+  const submenuToogle = () => {
+    setSubmenu((prevState) => !prevState);
+  };
 
   return (
     <>
@@ -49,70 +56,49 @@ const Nav = () => {
           </div>
 
           <ul className="buy-button list-none mb-0">
-            <li className="inline mb-0 ">
+            <li className="inline mb-0">
               <a
-                href=""
-                className="h-8 w-8 inline-flex items-center justify-center tracking-wide align-middle duration-500 text-[15px] text-center rounded-xl bg-blue-500 hover:bg-blue-600 border border-blue-500 hover:border-blue-600 text-white"
+                href="https://www.linkedin.com/in/soumen-bera-7a164a26b"
+                target="_blank"
+                className="h-8 w-8 inline-flex items-center justify-center tracking-wide align-middle duration-500 text-[15px] text-center rounded-xl bg-[#0A66C2] hover:bg-[#0A66C2] border border-[#0A66C2] hover:border-[#0A66C2] text-white"
               >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  className="feather feather-dribbble h-[14px] w-[14px] align-middle"
-                >
-                  <circle cx="12" cy="12" r="10"></circle>
-                  <path d="M8.56 2.75c4.37 6.03 6.02 9.42 8.03 17.72m2.54-15.38c-3.72 4.35-8.94 5.66-16.88 5.85m19.5 1.9c-3.5-.93-6.63-.82-8.94 0-2.58.92-5.01 2.86-7.44 6.32"></path>
-                </svg>
+                <FaLinkedinIn />
               </a>
             </li>
+
+            <li className="inline mb-0 mx-1">
+              <a
+                href="https://www.instagram.com/sledi_tz"
+                target="_blank"
+                className="h-8 w-8 inline-flex items-center justify-center tracking-wide align-middle duration-500 text-[15px] text-center rounded-xl bg-[#E1306C] hover:bg-[#E1306C] border border-[#E1306C] hover:border-[#E1306C] text-white"
+              >
+                <FaInstagram />
+              </a>
+            </li>
+            <li className="inline mb-0 mx-0">
+              <a
+                href="mailto:Sleditz87@gmail.com"
+                className="h-8 w-8 inline-flex items-center justify-center tracking-wide align-middle duration-500 text-[15px] text-center rounded-xl bg-[#545454] hover:bg-[#545454] border border-[#545454] hover:border-[#545454] text-white"
+              >
+                <MdEmail />
+              </a>
+            </li>
+
             <li className="inline mb-0 mx-1">
               <a
                 href=""
-                className="h-8 w-8 inline-flex items-center justify-center tracking-wide align-middle duration-500 text-[15px] text-center rounded-xl bg-blue-500 hover:bg-blue-600 border border-blue-500 hover:border-blue-600 text-white"
+                className="h-8 w-8 inline-flex items-center justify-center tracking-wide align-middle duration-500 text-[15px] text-center rounded-xl bg-[#316FF6] hover:bg-[#316FF6] border border-[#316FF6] hover:border-[#316FF6] text-white"
               >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  className="feather feather-linkedin h-[14px] w-[14px] align-middle"
-                >
-                  <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"></path>
-                  <rect x="2" y="9" width="4" height="12"></rect>
-                  <circle cx="4" cy="4" r="2"></circle>
-                </svg>
+                <FaFacebookF />
               </a>
             </li>
-            <li className="inline mb-0 ">
+            <li className="inline mb-0 mx-0">
               <a
-                href=""
-                className="h-8 w-8 inline-flex items-center justify-center tracking-wide align-middle duration-500 text-[15px] text-center rounded-xl bg-blue-500 hover:bg-blue-600 border border-blue-500 hover:border-blue-600 text-white"
+                href="https://www.youtube.com/@SlEditz_official"
+                target="_blank"
+                className="h-8 w-8 inline-flex items-center justify-center tracking-wide align-middle duration-500 text-[15px] text-center rounded-xl bg-[#ff4343] hover:bg-[#ff4343] border border-[#ff4343] hover:border-[#ff4343] text-white"
               >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  className="feather feather-github h-[14px] w-[14px] align-middle"
-                >
-                  <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
-                </svg>
+                <FaYoutube />
               </a>
             </li>
           </ul>
@@ -122,7 +108,6 @@ const Nav = () => {
             style={{ display: mobileNav ? "block" : "none" }}
           >
             <ul className="navigation-menu justify-end">
-
               <li className="active">
                 <a href="#" className="sub-menu-item">
                   Home
@@ -141,10 +126,13 @@ const Nav = () => {
                 </a>
               </li>
 
-              <li className="has-submenu parent-menu-item " onClick={submenuToogle}>
+              <li
+                className="has-submenu parent-menu-item "
+                onClick={submenuToogle}
+              >
                 <a href="#">Services</a>
                 <span className="menu-arrow"></span>
-                <ul className={submenu?"submenu open":"submenu"}>
+                <ul className={submenu ? "submenu open" : "submenu"}>
                   <li>
                     <a href="#" className="sub-menu-item">
                       Video Editing
--- src/components/SocialButton/SocialButton.tsx
+++ src/components/SocialButton/SocialButton.tsx
@@ -1,10 +1,11 @@
+import "./socialbutton.css";
+
 import { FaFacebookF } from "react-icons/fa";
 import { FaLinkedinIn } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa";
-import { FaXTwitter } from "react-icons/fa6";
 import { FaYoutube } from "react-icons/fa6";
+import { MdEmail } from "react-icons/md";
 
-import "./socialbutton.css";
 const ThemeToggle = () => {
   return (
     <>
@@ -12,29 +13,35 @@ const ThemeToggle = () => {
         <span className="relative inline-block">
           <ul className="social">
             <li className="bg-[#0A66C2]">
-              <a href="#">
+              <a
+                href="https://www.linkedin.com/in/soumen-bera-7a164a26b"
+                target="_blank"
+              >
                 <FaLinkedinIn className="fa" />
               </a>
             </li>
-
-            <li className=" bg-[#000000]">
-              <a href="#">
-                <FaXTwitter className="fa" />
-              </a>
-            </li>
-
             <li className="bg-[#E1306C]">
-              <a href="#">
+              <a href="https://www.instagram.com/sledi_tz" target="_blank">
                 <FaInstagram className="fa" />
               </a>
             </li>
-            <li className="bg-[#316FF6]">
-              <a href="#">
-                <FaFacebookF className="fa" />
+            <li className="bg-[#545454]">
+              <a href="mailto:Sleditz87@gmail.com">
+                <MdEmail className="fa" />
               </a>
             </li>
-            <li className="bg-[#CD201F]">
+
+            <li className="bg-[#316FF6]">
               <a href="#">
+                <FaFacebookF className="fa"/>
+              </a>
+            </li>
+
+            <li className="bg-[#ff4343]">
+              <a
+                href="https://www.youtube.com/@SlEditz_official"
+                target="_blank"
+              >
                 <FaYoutube className="fa" />
               </a>
             </li>
