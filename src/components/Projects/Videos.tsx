7b535d5c4e3397fd4065c1f935f84a5abd4614c7
adhikari-amit <adhikariamit2001@gmail.com>
2024-02-10 23:17:41 +0530

    minor change

--- src/components/Projects/Videos.tsx
+++ src/components/Projects/Videos.tsx
@@ -26,7 +26,6 @@ const Videos = () => {
                 height="300"
                 src="https://www.youtube.com/embed/Ze7wjUXVAMY?si=koWeQkjPzOrSZUM5"
                 title="YouTube video player"
-              
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                 allowFullScreen
               ></iframe>
@@ -39,11 +38,10 @@ const Videos = () => {
           <div className="group relative block rounded-xl duration-700 ease-in-out">
             <div className="relative overflow-hidden rounded-xl">
               <iframe
-                height="300"
                 width="100%"
-                src="https://www.youtube.com/embed/ql2crYEVrMY?si=Xp_wXdztQxm8-Jgx"
+                height="300"
+                src="https://www.youtube.com/embed/lLc4a4nZv4E?si=_veWdX0I-FNtPeh4"
                 title="YouTube video player"
-                className="rounded-xl"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                 allowFullScreen
               ></iframe>
@@ -55,7 +53,14 @@ const Videos = () => {
         <div className="lg:w-1/2 md:w-1/3 p-3 picture-item animate">
           <div className="group relative block rounded-xl duration-700 ease-in-out">
             <div className="relative overflow-hidden rounded-xl">
-            <iframe width="100%" height="300" src="https://www.youtube.com/embed/LVbUNRwpXzw?si=4hDNrqm5eIYUFZiG" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+              <iframe
+                width="100%"
+                height="300"
+                src="https://www.youtube.com/embed/LVbUNRwpXzw?si=4hDNrqm5eIYUFZiG"
+                title="YouTube video player"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                allowFullScreen
+              ></iframe>
             </div>
             <div className="absolute inset-0 bg-blue-500 rounded-xl group-hover:-mt-[10px] group-hover:-ms-[10px] h-[98%] w-[98%] duration-700 -z-1"></div>
           </div>
