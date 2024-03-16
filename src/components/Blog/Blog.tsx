33a4e8f9c62fb8f9d3dcbf3c84bf66bbe6361dee
adhikari-amit <adhikariamit2001@gmail.com>
2024-03-16 16:46:26 +0530

    Blogs added

--- src/components/Blog/Blog.tsx
+++ src/components/Blog/Blog.tsx
@@ -1,56 +1,45 @@
+import b1 from "../../assets/articles/1.png";
+import b2 from "../../assets/articles/2.jpg";
+import b3 from "../../assets/articles/3.jpg";
+
 const Blog = () => {
   return (
     <>
       <div className="container relative md:mt-24 mt-16" id="blogs">
         <div className="grid grid-cols-1 pb-6 text-center">
           <h3 className="mb-6 md:text-2xl text-xl font-medium">
-            Blogs &amp; News
+            Articles &amp; News
           </h3>
 
           <p className="text-slate-400 dark:text-white/60 max-w-xl mx-auto">
-            This is just a simple text made for this unique and awesome
-            template, you can replace it with any text.
+            Explore the latest articles published my me.
           </p>
         </div>
 
         <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 mt-6 gap-6">
           <div className="group">
             <div className="relative overflow-hidden rounded-xl shadow-md dark:shadow-gray-700">
-              <img src="https://source.unsplash.com/random/400×300/?books" alt="" className="w-[400px] h-[300px]"/>
+              <img src={b1} alt="" className="w-[400px] h-[300px]" />
               <div className="absolute inset-0 bg-gradient-to-b to-slate-900 from-transparent opacity-0 group-hover:opacity-100 duration-500"></div>
-              <div className="absolute start-4 bottom-4 opacity-0 group-hover:opacity-100 flex items-center duration-500">
-                <img
-                  src="https://source.unsplash.com/random/400×300/?books"
-                  className="h-9 w-9 rounded-xl"
-                  alt=""
-                />
-                <div className="ms-2">
-                  <a
-                    href=""
-                    className="text-white hover:text-blue-500 text-[15px]"
-                  >
-                    Calvin Carlo
-                  </a>
-                  <p className="text-white/70 text-sm">5 Min Read</p>
-                </div>
-              </div>
             </div>
 
             <div className="p-4">
               <a
-                href="https://amamit.medium.com/firebase-integration-with-react-3f5c74316e0f"
+                href="https://www.linkedin.com/pulse/revolutionizing-video-editing-sora-ai-redefining-creativity-bera-kl8vc"
                 className="title text-lg font-medium hover:text-blue-500 duration-500"
               >
-                Design your apps in your own way
+                Revolutionizing Video Editing with 'Sora AI' : Redefining
+                Creativity and Efficiency
               </a>
               <p className="text-slate-400 dark:text-white/60 mt-3">
-                The phrasal sequence of the is now so that many campaign and
-                benefit
+                Are you ready to revolutionize your video editing experience?
+                Explore the possibilities with Sora AI and unleash your
+                creativity like never before.
               </p>
 
               <div className="mt-3">
                 <a
-                  href="https://amamit.medium.com/firebase-integration-with-react-3f5c74316e0f"
+                  href="https://www.linkedin.com/pulse/revolutionizing-video-editing-sora-ai-redefining-creativity-bera-kl8vc"
                   className="hover:text-blue-500"
                 >
                   Read More <i className="mdi mdi-arrow-right align-middle"></i>
@@ -61,41 +50,26 @@ const Blog = () => {
 
           <div className="group">
             <div className="relative overflow-hidden rounded-xl shadow-md dark:shadow-gray-700">
-              <img src="https://source.unsplash.com/random/400×300/?city,night" alt="" className="w-[400px] h-[300px]" />
-              <div className="absolute inset-0 bg-gradient-to-b to-slate-900 from-transparent opacity-0 group-hover:opacity-100 duration-500"></div>
-              <div className="absolute start-4 bottom-4 opacity-0 group-hover:opacity-100 flex items-center duration-500">
-                <img
-                  src="https://source.unsplash.com/random/400×300/?city,night"
-                  className="h-9 w-9 rounded-xl"
-                  alt=""
-                />
-                <div className="ms-2">
-                  <a
-                    href=""
-                    className="text-white hover:text-blue-500 text-[15px]"
-                  >
-                    Calvin Carlo
-                  </a>
-                  <p className="text-white/70 text-sm">5 Min Read</p>
-                </div>
-              </div>
+              <img src={b2} alt="" className="w-[400px] h-[300px]" />
             </div>
 
             <div className="p-4">
               <a
-                href="https://amamit.medium.com/firebase-integration-with-react-3f5c74316e0f"
+                href="https://www.linkedin.com/pulse/balancing-act-nurturing-health-work-life-harmony-freelance-bera-vadsc"
                 className="title text-lg font-medium hover:text-blue-500 duration-500"
               >
-                How apps is changing the IT world
+                Balancing Act: Nurturing Health and Work-Life Harmony in the
+                Freelance Video Editing World
               </a>
               <p className="text-slate-400 dark:text-white/60 mt-3">
-                The phrasal sequence of the is now so that many campaign and
-                benefit
+                As we navigate the challenges of freelance video editing in
+                2024, let's collectively commit to fostering a culture that
+                values health and work-life balance.
               </p>
 
               <div className="mt-3">
                 <a
-                  href="https://amamit.medium.com/firebase-integration-with-react-3f5c74316e0f"
+                  href="https://www.linkedin.com/pulse/balancing-act-nurturing-health-work-life-harmony-freelance-bera-vadsc"
                   className="hover:text-blue-500"
                 >
                   Read More <i className="mdi mdi-arrow-right align-middle"></i>
@@ -106,42 +80,26 @@ const Blog = () => {
 
           <div className="group">
             <div className="relative overflow-hidden rounded-xl shadow-md dark:shadow-gray-700">
-              <img src="https://source.unsplash.com/random/400×300/?women" className="w-[400px] h-[300px]" alt="" />
+              <img src={b3} className="w-[400px] h-[300px]" alt="" />
               <div className="absolute inset-0 bg-gradient-to-b to-slate-900 from-transparent opacity-0 group-hover:opacity-100 duration-500"></div>
-              <div className="absolute start-4 bottom-4 opacity-0 group-hover:opacity-100 flex items-center duration-500">
-                <img
-                  src="https://source.unsplash.com/random/400×300/?women"
-                  className="h-9 w-9 rounded-xl"
-                  alt=""
-                  
-                />
-                <div className="ms-2">
-                  <a
-                    href=""
-                    className="text-white hover:text-blue-500 text-[15px]"
-                  >
-                    Calvin Carlo
-                  </a>
-                  <p className="text-white/70 text-sm">5 Min Read</p>
-                </div>
-              </div>
             </div>
 
             <div className="p-4">
               <a
-                href="https://amamit.medium.com/firebase-integration-with-react-3f5c74316e0f"
+                href="https://www.linkedin.com/pulse/unlocking-career-opportunities-power-video-editing-todays-soumen-bera-w4oic"
                 className="title text-lg font-medium hover:text-blue-500 duration-500"
               >
-                Smartest Applications for Business
+                Unlocking Career Opportunities: The Power of Video Editing in
+                Today's Digital Landscape.
               </a>
               <p className="text-slate-400 dark:text-white/60 mt-3">
-                The phrasal sequence of the is now so that many campaign and
-                benefit
+                he world of video editing is brimming with opportunities for
+                those who are passionate, creative, and willing to adapt.
               </p>
 
               <div className="mt-3">
                 <a
-                  href="https://amamit.medium.com/firebase-integration-with-react-3f5c74316e0f"
+                  href="https://www.linkedin.com/pulse/unlocking-career-opportunities-power-video-editing-todays-soumen-bera-w4oic"
                   className="hover:text-blue-500"
                 >
                   Read More <i className="mdi mdi-arrow-right align-middle"></i>
