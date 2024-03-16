c7a7c5d09150bcb29e211240b4301a861738dc11
adhikari-amit <adhikariamit2001@gmail.com>
2024-03-16 16:23:57 +0530

    Reviews added

--- src/components/Review/Review.tsx
+++ src/components/Review/Review.tsx
@@ -14,53 +14,7 @@ const Review = () => {
         </div>
 
         <div className="grid lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 mt-6 gap-6">
-          
           <ul className="space-y-6">
-            <li className="rounded-xl shadow dark:shadow-gray-800 p-6 bg-white dark:bg-slate-900">
-              <div className="flex items-center pb-6 border-b border-gray-100 dark:border-gray-800">
-                <img
-                  src="https://avatar.iran.liara.run/public/boy"
-                  className="h-14 w-14 rounded-full shadow dark:shadow-gray-800"
-                  alt=""
-                />
-
-                <div className="ps-3">
-                  <a
-                    href=""
-                    className="text-[16px] font-medium hover:text-blue-500 duration-500"
-                  >
-                    Thomas Israel
-                  </a>
-                  <p className="text-slate-400 dark:text-white/60">C.E.O</p>
-                </div>
-              </div>
-
-              <div className="mt-6">
-                <p className="text-slate-400 dark:text-white/60">
-                  I didn't know a thing about icon design until I read this
-                  book. Now I can create any icon I need in no time. Great
-                  resource!
-                </p>
-                <ul className="list-none mb-0 text-amber-400 mt-2">
-                  <li className="inline">
-                    <i className="mdi mdi-star"></i>
-                  </li>
-                  <li className="inline">
-                    <i className="mdi mdi-star"></i>
-                  </li>
-                  <li className="inline">
-                    <i className="mdi mdi-star"></i>
-                  </li>
-                  <li className="inline">
-                    <i className="mdi mdi-star"></i>
-                  </li>
-                  <li className="inline">
-                    <i className="mdi mdi-star"></i>
-                  </li>
-                </ul>
-              </div>
-            </li>
-
             <li className="rounded-xl shadow dark:shadow-gray-800 p-6 bg-white dark:bg-slate-900">
               <div className="flex items-center pb-6 border-b border-gray-100 dark:border-gray-800">
                 <img
@@ -74,17 +28,110 @@ const Review = () => {
                     href=""
                     className="text-[16px] font-medium hover:text-blue-500 duration-500"
                   >
-                    Barbara McIntosh
+                    Emma Thompson
                   </a>
-                  <p className="text-slate-400 dark:text-white/60">C.E.O</p>
+                  <p className="text-slate-400 dark:text-white/60">
+                    CEO, BrightHorizon Media.
+                  </p>
                 </div>
               </div>
 
               <div className="mt-6">
                 <p className="text-slate-400 dark:text-white/60">
-                  There are so many things I had to do with my old software that
-                  I just don't do at all with Techwind. Suspicious but I can't
-                  say I don't love it.
+                  Working with SlEditz was an absolute delight! Their video
+                  editing skills brought our project to life in ways we hadn't
+                  imagined.
+                </p>
+                <ul className="list-none mb-0 text-amber-400 mt-2">
+                  <li className="inline">
+                    <i className="mdi mdi-star"></i>
+                  </li>
+                  <li className="inline">
+                    <i className="mdi mdi-star"></i>
+                  </li>
+                  <li className="inline">
+                    <i className="mdi mdi-star"></i>
+                  </li>
+                  <li className="inline">
+                    <i className="mdi mdi-star"></i>
+                  </li>
+                  <li className="inline">
+                    <i className="mdi mdi-star"></i>
+                  </li>
+                </ul>
+              </div>
+            </li>
+
+
+           
+            <li className="rounded-xl shadow dark:shadow-gray-800 p-6 bg-white dark:bg-slate-900">
+              <div className="flex items-center pb-6 border-b border-gray-100 dark:border-gray-800">
+                <img
+                  src="https://avatar.iran.liara.run/public/boy"
+                  className="h-14 w-14 rounded-full shadow dark:shadow-gray-800"
+                  alt=""
+                />
+
+                <div className="ps-3">
+                  <a
+                    href=""
+                    className="text-[16px] font-medium hover:text-blue-500 duration-500"
+                  >
+                    James Anderson
+                  </a>
+                  <p className="text-slate-400 dark:text-white/60">
+                    Marketing Manager, VisionaryTech
+                  </p>
+                </div>
+              </div>
+
+              <div className="mt-6">
+                <p className="text-slate-400 dark:text-white/60">
+                  I can't recommend SlEditz enough! Their attention to detail
+                  and creative approach transformed our video content.
+                </p>
+                <ul className="list-none mb-0 text-amber-400 mt-2">
+                  <li className="inline">
+                    <i className="mdi mdi-star"></i>
+                  </li>
+                  <li className="inline">
+                    <i className="mdi mdi-star"></i>
+                  </li>
+                  <li className="inline">
+                    <i className="mdi mdi-star"></i>
+                  </li>
+                  <li className="inline">
+                    <i className="mdi mdi-star"></i>
+                  </li>
+                  <li className="inline">
+                    <i className="mdi mdi-star"></i>
+                  </li>
+                </ul>
+              </div>
+            </li>
+
+            <li className="rounded-xl shadow dark:shadow-gray-800 p-6 bg-white dark:bg-slate-900">
+              <div className="flex items-center pb-6 border-b border-gray-100 dark:border-gray-800">
+                <img
+                  src="https://avatar.iran.liara.run/public/boy?username=benjamin"
+                  className="h-14 w-14 rounded-full shadow dark:shadow-gray-800"
+                  alt=""
+                />
+
+                <div className="ps-3">
+                  <a
+                    href=""
+                    className="text-[16px] font-medium hover:text-blue-500 duration-500"
+                  >
+                   Benjamin Foster 
+                  </a>
+                  <p className="text-slate-400 dark:text-white/60"> Creative Lead, FusionTech Media.</p>
+                </div>
+              </div>
+
+              <div className="mt-6">
+                <p className="text-slate-400 dark:text-white/60">
+                Working with SlEditz was a game-changer for our video content. The level of creativity and professionalism they brought to the table was outstanding.
                 </p>
                 <ul className="list-none mb-0 text-amber-400 mt-2">
                   <li className="inline">
@@ -111,7 +158,7 @@ const Review = () => {
             <li className="rounded-xl shadow dark:shadow-gray-800 p-6 bg-white dark:bg-slate-900">
               <div className="flex items-center pb-6 border-b border-gray-100 dark:border-gray-800">
                 <img
-                  src="https://avatar.iran.liara.run/public/boy?age=20"
+                  src="https://avatar.iran.liara.run/public/girl?username=sarah"
                   className="h-14 w-14 rounded-full shadow dark:shadow-gray-800"
                   alt=""
                 />
@@ -121,18 +168,18 @@ const Review = () => {
                     href=""
                     className="text-[16px] font-medium hover:text-blue-500 duration-500"
                   >
-                    Carl Oliver
+                    Sarah Williams
                   </a>
-                  <p className="text-slate-400 dark:text-white/60">C.E.O</p>
+                  <p className="text-slate-400 dark:text-white/60">
+                    Founder, Spark Innovations
+                  </p>
                 </div>
               </div>
 
               <div className="mt-6">
                 <p className="text-slate-400 dark:text-white/60">
-                  The best part about Techwind is every time I pay my employees,
-                  my bank balance doesn't go down like it used to. Looking
-                  forward to spending this extra cash when I figure out why my
-                  card is being declined.
+                  SlEditz is a video editing wizard! The seamless transitions
+                  and storytelling in our videos exceeded our expectations.
                 </p>
                 <ul className="list-none mb-0 text-amber-400 mt-2">
                   <li className="inline">
@@ -157,8 +204,7 @@ const Review = () => {
             <li className="rounded-xl shadow dark:shadow-gray-800 p-6 bg-white dark:bg-slate-900">
               <div className="flex items-center pb-6 border-b border-gray-100 dark:border-gray-800">
                 <img
-                  src="https://avatar.iran.liara.run/public/girl?username=tom"
-                  
+                  src="https://avatar.iran.liara.run/public/boy?username=Michael"
                   className="h-14 w-14 rounded-full shadow dark:shadow-gray-800"
                   alt=""
                 />
@@ -168,17 +214,61 @@ const Review = () => {
                     href=""
                     className="text-[16px] font-medium hover:text-blue-500 duration-500"
                   >
-                    Jill Webb
+                    Michael Rodriguez
                   </a>
-                  <p className="text-slate-400 dark:text-white/60">C.E.O</p>
+                  <p className="text-slate-400 dark:text-white/60">
+                    Creative Director, BoldCatalyst Studios.
+                  </p>
                 </div>
               </div>
 
               <div className="mt-6">
                 <p className="text-slate-400 dark:text-white/60">
-                  I'm trying to get a hold of someone in support, I'm in a lot
-                  of trouble right now and they are saying it has something to
-                  do with my books. Please get back to me right away.
+                  Outstanding work by SlEditz ! Their expertise in video editing
+                  elevated our brand's visual appeal.
+                </p>
+                <ul className="list-none mb-0 text-amber-400 mt-2">
+                  <li className="inline">
+                    <i className="mdi mdi-star"></i>
+                  </li>
+                  <li className="inline">
+                    <i className="mdi mdi-star"></i>
+                  </li>
+                  <li className="inline">
+                    <i className="mdi mdi-star"></i>
+                  </li>
+                  <li className="inline">
+                    <i className="mdi mdi-star"></i>
+                  </li>
+                  <li className="inline">
+                    <i className="mdi mdi-star"></i>
+                  </li>
+                </ul>
+              </div>
+            </li>
+
+            <li className="rounded-xl shadow dark:shadow-gray-800 p-6 bg-white dark:bg-slate-900">
+              <div className="flex items-center pb-6 border-b border-gray-100 dark:border-gray-800">
+                <img
+                  src="https://avatar.iran.liara.run/public/girl?username=olivia"
+                  className="h-14 w-14 rounded-full shadow dark:shadow-gray-800"
+                  alt=""
+                />
+
+                <div className="ps-3">
+                  <a
+                    href=""
+                    className="text-[16px] font-medium hover:text-blue-500 duration-500"
+                  >
+                   Olivia Martinez 
+                  </a>
+                  <p className="text-slate-400 dark:text-white/60">CEO, Harmony Productions.</p>
+                </div>
+              </div>
+
+              <div className="mt-6">
+                <p className="text-slate-400 dark:text-white/60">
+                Professional, efficient, and incredibly creative – that's SlEditz for you! Our video content has never looked better.  
                 </p>
                 <ul className="list-none mb-0 text-amber-400 mt-2">
                   <li className="inline">
@@ -205,7 +295,7 @@ const Review = () => {
             <li className="rounded-xl shadow dark:shadow-gray-800 p-6 bg-white dark:bg-slate-900">
               <div className="flex items-center pb-6 border-b border-gray-100 dark:border-gray-800">
                 <img
-                  src="https://avatar.iran.liara.run/public/boy?username=amit"
+                  src="https://avatar.iran.liara.run/public/girl?username=foster"
                   className="h-14 w-14 rounded-full shadow dark:shadow-gray-800"
                   alt=""
                 />
@@ -215,17 +305,15 @@ const Review = () => {
                     href=""
                     className="text-[16px] font-medium hover:text-blue-500 duration-500"
                   >
-                    Barbara McIntosh
+                    Emily Foster 
                   </a>
-                  <p className="text-slate-400 dark:text-white/60">C.E.O</p>
+                  <p className="text-slate-400 dark:text-white/60">Content Manager, SkyHigh Ventures</p>
                 </div>
               </div>
 
               <div className="mt-6">
                 <p className="text-slate-400 dark:text-white/60">
-                  I used to have to remit tax to the EU and with Techwind I
-                  somehow don't have to do that anymore. Nervous to travel there
-                  now though.
+                An absolute pleasure to collaborate with SlEditz .Their video editing prowess turned our ideas into cinematic reality.
                 </p>
                 <ul className="list-none mb-0 text-amber-400 mt-2">
                   <li className="inline">
@@ -260,17 +348,58 @@ const Review = () => {
                     href=""
                     className="text-[16px] font-medium hover:text-blue-500 duration-500"
                   >
-                    Janisha Doll
+                   Daniel Chen 
                   </a>
-                  <p className="text-slate-400 dark:text-white/60">C.E.O</p>
+                  <p className="text-slate-400 dark:text-white/60">Director of Marketing, LunaTech.</p>
                 </div>
               </div>
 
               <div className="mt-6">
                 <p className="text-slate-400 dark:text-white/60">
-                  This is the fourth email I've sent to your support team. I am
-                  literally being held in jail for tax fraud. Please answer your
-                  damn emails, this is important.
+                SlEditz brought a unique flair to our project. The combination of technical skill and artistic vision resulted in outstanding videos. 
+                </p>
+                <ul className="list-none mb-0 text-amber-400 mt-2">
+                  <li className="inline">
+                    <i className="mdi mdi-star"></i>
+                  </li>
+                  <li className="inline">
+                    <i className="mdi mdi-star"></i>
+                  </li>
+                  <li className="inline">
+                    <i className="mdi mdi-star"></i>
+                  </li>
+                  <li className="inline">
+                    <i className="mdi mdi-star"></i>
+                  </li>
+                  <li className="inline">
+                    <i className="mdi mdi-star"></i>
+                  </li>
+                </ul>
+              </div>
+            </li>
+
+            <li className="rounded-xl shadow dark:shadow-gray-800 p-6 bg-white dark:bg-slate-900">
+              <div className="flex items-center pb-6 border-b border-gray-100 dark:border-gray-800">
+                <img
+                  src="https://avatar.iran.liara.run/public/boy?username=Ryan"
+                  className="h-14 w-14 rounded-full shadow dark:shadow-gray-800"
+                  alt=""
+                />
+
+                <div className="ps-3">
+                  <a
+                    href=""
+                    className="text-[16px] font-medium hover:text-blue-500 duration-500"
+                  >
+                  Ryan Turner  
+                  </a>
+                  <p className="text-slate-400 dark:text-white/60">Founder, PixelCraft Studios.</p>
+                </div>
+              </div>
+
+              <div className="mt-6">
+                <p className="text-slate-400 dark:text-white/60">
+                I can't express how pleased we are with SlEditz's video editing services. They understand the pulse of our brand and deliver exceptional results.
                 </p>
                 <ul className="list-none mb-0 text-amber-400 mt-2">
                   <li className="inline">
@@ -292,12 +421,8 @@ const Review = () => {
               </div>
             </li>
           </ul>
-
         </div>
       </div>
-
-   
-
     </>
   );
 };
