d7a7ed02670e89ac289d017cb30efdfec0d7f9f9
adhikari-amit <adhikariamit2001@gmail.com>
2024-03-17 19:08:38 +0530

    Optimization

--- src/components/Preloader/Preloader.tsx
+++ src/components/Preloader/Preloader.tsx
@@ -1,5 +1,5 @@
 import "./Preloader.css";
-import loadingGif from "../../assets/img/loader.gif";
+import loadingGif from "../../assets/img/loader1.gif";
 
 const Preloader = () => {
   return (
@@ -8,11 +8,11 @@ const Preloader = () => {
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
-        height:"100vh",
+        
         
       }}
     >
-      <img src={loadingGif} alt="" style={{ width: "50vw" }} />
+      <img src={loadingGif} alt=""  />
     </div>
   );
 };
